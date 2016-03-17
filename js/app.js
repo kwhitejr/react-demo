db89ba7e7f98992867bd6ebf8bce878080dd98ac
kwhitejr <kwhitejr.edu@gmail.com>
2016-03-16 19:50:16 -1000

    rendering comments; implementing markup

--- js/app.js
+++ js/app.js
@@ -1,1 +1,61 @@
-document.write('hello world!');
+/**
+  * - CommentBox
+  *   - CommentList
+  *     - Comment
+  *   - CommentForm
+**/
+
+var CommentBox = React.createClass({
+  render: function () {
+    return (
+      <div className="commentBox">
+        Hello world! I am a CommentBox.
+        <CommentList />
+        <CommentForm />
+      </div>
+    );
+  }
+});
+
+var CommentList = React.createClass({
+  render: function () {
+    return (
+      <div className="commentList">
+        <Comment author="Kevin">React is my jam.</Comment>
+        <Comment author="Jon">React is _**my**_ jam.</Comment>
+      </div>
+    )
+  }
+})
+
+var Comment = React.createClass({
+  rawMarkup: function () {
+    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+    return { __html: rawMarkup };
+  },
+  render: function () {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
+      </div>
+    )
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function () {
+    return (
+      <div className="commentForm">
+        This isn't even my final FORM.
+      </div>
+    )
+  }
+})
+
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
